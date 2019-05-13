74b8108f6ceab551979fe930defabfed22213cbe
NickBirks <birknic@bvu.edu>
2019-05-13 14:01:04 -0500

    trying our best

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import abort, Flask, json, redirect,\
+    render_template, request, Response, url_for, session, jsonify
+from werkzeug.utils import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = app.config['SECRET_KEY']
+db = SQLAlchemy(app)
+
+
+# Import any SQLAlchemy model classes you wish.
+#from models import FIXME
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
